b81001e1f6cb29e5132dde0c2d507830f52742c4
Campbell McDiarmid <campbell.mcdiarmid@pg.canterbury.ac.nz>
2019-04-02 13:40:57 +1300

    Changed while loop conditions to terminate upon .close() and close sockets. Printing/debugging code tidied

--- PX4_to_XBee.py
+++ PX4_to_XBee.py
@@ -1,43 +1,71 @@
 from digi.xbee.devices import XBeeDevice
 import serial, time
+import serial.tools.list_ports as list_ports
+
+# Constants used throughout script
+XBEE_PKT_MAX = 0xFF
+SERIAL_BUFFER_MAX = 0xFFF
+ARBITRARY_MIN = 100
+XBEE_MAX_BAUD = 230400
+PX4_DEFAULT_BAUD = 115200
+
+
+def device_finder(name):
+	devport = None
+	while devport is None:
+		for dev in list_ports.comports():
+			if name in dev.product:
+				devport = dev.device
+		if devport:
+			print('{} found'.format(name))
+		else:
+			print('Please insert {} device'.format(name))
+			time.sleep(5)
+	return devport
+
 
 def main():
-	# TODO: Add command line argument passing 
-	px4 = serial.Serial('/dev/ttyACM0', 115200)
-	xb = XBeeDevice('/dev/ttyUSB0', 230400)
+	# TODO: Add command line argument passing
+	# Find PX4 Device and open a serial connection
+	px4port = device_finder('PX4')
+	px4 = serial.Serial(px4port, PX4_DEFAULT_BAUD)
+
+	# Find XBee Device and open a serial connection
+	xbeeport = device_finder('XBee')
+	xb = XBeeDevice(xbeeport, XBEE_MAX_BAUD)
 	xb.open()
 
-	network = xb.get_network()
-	network.start_discovery_process()
-
-	while network.is_discovery_running():
-		time.sleep(0.1)
-
+	# Discover network.  Repeat until GCS has been found.
 	GCS = None
+	network = xb.get_network()
+	while GCS is None:
+		print('Discovering network, GCS not found yet.')
+		network.start_discovery_process()
+		while network.is_discovery_running():
+			time.sleep(0.1)
+		for device in network.get_devices():
+			print('UAV found with Node ID: {}'.format(device.get_node_id()))
+			if device.get_node_id() == 'GCS':
+				GCS = device
 
-	for device in network.get_devices():
-		if device.get_node_id() == 'GCS':
-			GCS = device
-
-	if GCS is None:
-		raise Exception('Ground Control Station not discovered.')
-
+	# Infinite loop for bridging serial connection between Pixhawk and XBee
 	while True:
 		# Read PX4, Write to XBee
-		if 100 <= px4.in_waiting < 4095:
+		if ARBITRARY_MIN <= px4.in_waiting < SERIAL_BUFFER_MAX:
 			data = px4.read(px4.in_waiting)
+			# Break data up into packets of maximum length XBEE_PKT_MAX
 			while data:
-				print('[PX4 -> XBee] {}'.format(data[:255]))
-				pkt_sent = xb.send_data(GCS, data[:255])
-				data = data[255:]
-		elif px4.in_waiting == 4095:
+				print('[PX4 -> XBee] {}'.format(data[:XBEE_PKT_MAX]))
+				pkt_sent = xb.send_data(GCS, data[:XBEE_PKT_MAX])
+				data = data[XBEE_PKT_MAX:]
+		elif px4.in_waiting == SERIAL_BUFFER_MAX:
+			# Reset input buffer if overflow has occured
 			px4.reset_input_buffer()
-		else:
-			pass
 
 		# Read XBee, Write to PX4
 		message = xb.read_data_from(GCS)
 		if message:
+			# TODO: is there a maximum input size?
 			data = bytes(message.data)
 			print('[XBee -> PX4] {}'.format(data))
 			px4.write(data)
--- XBee_to_UDP.py
+++ XBee_to_UDP.py
@@ -1,26 +1,32 @@
 import socket, serial, time, datetime, threading, os
 from digi.xbee.devices import XBeeDevice
+from digi.xbee.exception import TimeoutException
 
-
-# Buffer values based off QGroundControl git in src/comm/UDPLink.cc, l. 298-299
-KiB = 1024
-BUFFER_LIMIT = 4095
 
 # Localhost IP and arbirtarily defined based port
+LOCALHOST = '127.0.0.1'
 UDP_PORT = 14555
-UDP_IP = '127.0.0.1'
+UDP_IP = LOCALHOST
 SCRIPT_START = time.time()
 
 # MAVLink constants
 HEADER_LEN = 6
 LEN_BYTE = 1
 CRC_LEN = 2
-START_BYTE = 0xfe
+START_BYTE = 0xFE
+
+# Other serial device constants
+XBEE_PKT_MAX = 0xFF
+XBEE_MAX_BAUD = 230400
+PX4_DEFAULT_BAUD = 115200
+KiB = 1024
+SER_BUF_LIMIT = 0xFFF
+
 
 # Fucntions and Classes
 class XBeeToUDP(XBeeDevice):
-    def __init__(self, serialport='/dev/ttyUSB0', baud_rate=230400,
-            udp=('127.0.0.1', 14555), **kwargs):
+    def __init__(self, serialport='/dev/ttyUSB0', baud_rate=XBEE_MAX_BAUD,
+            udp=(UDP_IP, UDP_PORT), **kwargs):
 
         # Initialize XBee device connection
         super().__init__(port=serialport, baud_rate=baud_rate, **kwargs)
@@ -29,6 +35,7 @@ class XBeeToUDP(XBeeDevice):
         self.udp_ip, self.udp_port = udp
         self.udp_connections = {}  # Port: (b'\xfe\x42... queue_in, queue_out)
         self.sockets = {}
+        self.running = True
 
 
     def start(self):
@@ -38,6 +45,10 @@ class XBeeToUDP(XBeeDevice):
         serial_thread = threading.Thread(target=self.primary_loop,
                                         daemon=True)
         serial_thread.start()
+
+    def close(self):
+        super().close()
+        self.running = False
 
 
     def primary_loop(self):
@@ -50,47 +61,64 @@ class XBeeToUDP(XBeeDevice):
             time.sleep(0.1)
         print("Finished: {}\n".format(mesh.get_devices()))
 
-        while True:
-            # Check for new packets from each remote XBee
-            for device in mesh.get_devices():
-                message =  self.read_data_from(device)
-                if message is not None:
-                    indata = bytes(message.data)
+        bytes_recv = 0
+        bytes_sent = 0
+        recv_start = time.time()
+        try:
+            while self.running:
+                # Check for new packets from each remote XBee
+                for device in mesh.get_devices():
+                    message =  self.read_data_from(device)
+                    if message is not None:
+                        indata = bytes(message.data)
+                        bytes_recv += len(indata)
+                        key = device.get_64bit_addr().address.hex()
+
+                        if key not in self.udp_connections:
+                            # Add new queue and spawn new UDP thread
+                            self.spawn_udp_thread(key)
+                            time.sleep(0.001)
+
+                        queued_data = self.udp_connections[key]['IN'] + indata
+                        # TODO: Semaphore/Mutex begin
+                        sent = self.sockets[key].send(queued_data)
+                        # TODO: Semaphore/Mutex end
+                        self.print_msg(key, incoming=queued_data[:sent])
+                        self.udp_connections[key]['IN'] = queued_data[sent:]
+
+                        time_elapsed = time.time() - recv_start
+                        in_rate = 8 * bytes_recv / time_elapsed / 1000
+                        print('Recv: {0:.2f}kbps'.format(in_rate))
+
+
+                # TODO Check general broadcasts
+
+                # Check for new items in the outgoing queue
+                for device in mesh.get_devices():
                     key = device.get_64bit_addr().address.hex()
 
                     if key not in self.udp_connections:
-                        # Add new queue and spawn new UDP thread
                         self.spawn_udp_thread(key)
-                        time.sleep(0.001)
 
-                    queued_data = self.udp_connections[key]['IN'] + indata
-                    # TODO: Semaphore/Mutex begin
-                    sent = self.sockets[key].send(queued_data)
-                    # TODO: Semaphore/Mutex end
-                    self.print_msg(key, incoming=queued_data[:sent])
-                    self.udp_connections[key]['IN'] = queued_data[sent:]
+                    # TODO: Semaphore/mutex?
+                    outdata = self.udp_connections[key]['OUT']
+                    if outdata:
+                        self.udp_connections[key]['OUT'] = self.udp_connections[key]['OUT'][len(outdata):]
+                        while outdata:
+                            pkt_sent = self.send_data(device, outdata[:XBEE_PKT_MAX])
+                            bytes_sent += len(outdata[:XBEE_PKT_MAX])
+                            self.print_msg(key, outgoing=outdata[:XBEE_PKT_MAX])
+                            outdata = outdata[XBEE_PKT_MAX:]
 
+                        time_elapsed = time.time() - recv_start
+                        out_rate = 8 * bytes_sent / time_elapsed / 1000
+                        print('Sent: {0:.2f}kbps'.format(out_rate))
 
-            # TODO Check general broadcasts
+                # End of while loop
+                time.sleep(0.001)
 
-            # Check for new items in the outgoing queue
-            for device in mesh.get_devices():
-                key = device.get_64bit_addr().address.hex()
-
-                if key not in self.udp_connections:
-                    self.spawn_udp_thread(key)
-
-                # TODO: Semaphore/mutex?
-                outdata = self.udp_connections[key]['OUT']
-                if outdata:
-                    self.udp_connections[key]['OUT'] = self.udp_connections[key]['OUT'][len(outdata):]
-                    while outdata:
-                        pkt_sent = self.send_data(device, outdata[:255])
-                        self.print_msg(key, outgoing=outdata[:255])
-                        outdata = outdata[255:]
-
-            # End of while loop
-            time.sleep(0.001)
+        except TimeoutException:
+                print('Exiting main loop')
 
 
     def udp_thread(self, key):
@@ -103,13 +131,14 @@ class XBeeToUDP(XBeeDevice):
         print('Created UDP link for wireless connection with {}'.format(key))
 
         # Perpetual loop for sending and receiving data to and from UDP conn
-        starttime = time.time()
-
-        while True:
+        while self.running:
             # Check for new data from UDP conn
             outdata, _ = sock.recvfrom(KiB)  # TODO blocking by default
             self.udp_connections[key]['OUT'] += outdata
             time.sleep(0.05)
+
+        sock.close()
+        print('Exiting thread for {}'.format(key))
 
     def spawn_udp_thread(self, key):
         print(key)
@@ -119,40 +148,38 @@ class XBeeToUDP(XBeeDevice):
 
     @staticmethod
     def print_msg(key, incoming=b'', outgoing=b''):
+        # Formats incoming and outgoing messages to fit the terminal window
         cols, rows = os.get_terminal_size()
         elapsed = time.time()-SCRIPT_START
-        lhs = 6
-        while elapsed >= 10:
+        lhs = 6  # Left hand side width in monospace unicode characters
+
+        while elapsed >= 10:  # Increase LHS for each digit of elapsed time
             elapsed %= 10
             lhs += 1
-        rhs = cols - lhs - 2
-        bpl = rhs // 4
+
+        rhs = cols - lhs - 2  # Right hand side width
+        bpl = rhs // 4  # Bytes displayed per line
+        brkline = '-' * cols + '\n'  # Line of hyphens
         line_format = '{' + f': <{lhs}' + '}| {' + f': <{rhs}' + '}\n'
-        txt = '-' * cols + '\n'
+        txt = brkline + line_format.format(f'{time.time()-SCRIPT_START:.2f}s', key)
+        sep = ', '
 
-        i = -1
-        while incoming:
-            if i == -1:
-                line = line_format.format(f'{time.time()-SCRIPT_START:.2f}s', key)
-            else:
-                line = line_format.format(f'IN[{i}]', ', '.join(r'%02X' % c for c  in incoming[:bpl]))
-                incoming = incoming[bpl:]
-            i += 1
-            txt += line
+        for direction, data in (('IN', incoming), ('OUT', outgoing)):
+            nlines, rem = divmod(len(data), bpl)
+            nlines += rem > 0
+            for i in range(nlines):
+                linedata = sep.join('%02X' % c for c  in data[i*bpl:(i+1)*bpl])
+                line = line_format.format(f'{direction}[{i}]', linedata)
+                txt += line
+            if nlines:
+                txt += brkline
 
-        i = 0
-        while outgoing:
-            line = line_format.format(f'OUT[{i}]', ', '.join(r'%02X' % c for c  in outgoing[:bpl]))
-            outgoing = outgoing[bpl:]
-            txt += line
-            i += 1
-
-        print(txt + '\n')
+        print(txt)
 
 
 if __name__ == '__main__':
     # TODO: Add command line argument passing
-    xb = XBeeToUDP('/dev/ttyUSB0', 230400)
+    xb = XBeeToUDP('/dev/ttyUSB0', XBEE_MAX_BAUD)
     xb.start()
 
     try:
@@ -160,4 +187,3 @@ if __name__ == '__main__':
             time.sleep(0.5)
     except (KeyboardInterrupt, SystemExit):
         xb.close()
-        print('Terminating all threads and closing all ports.\nExiting...')
